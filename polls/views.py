e6b07b709cf040731413a91ff0b39ce4efe53ee2
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-02-25 23:13:45 -0500

    notes added

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 
+#this is the function used to create the hhtp response for /polls
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
