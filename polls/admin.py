7ae066a9d9995c391d9d2007c9228d2bd5a549ff
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2021-02-26 14:13:06 -0500

    made Questions object accessible in the admin interface

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your model
+from django.contrib import admin
+from .models import Question
+
+admin.site.register(Question)
